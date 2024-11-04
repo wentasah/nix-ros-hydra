24538074a2d59df94d688859e7767cc8a8270844
Michal Sojka <michal.sojka@cvut.cz>
2024-11-04 23:25:04 +0100

    Add "unstable" jobsets
    This are the develop branch of nix-ros-overlay built against
    nixos-unstable branch of nixpkgs.

--- generate-jobsets.py
+++ generate-jobsets.py
@@ -3,7 +3,32 @@
 import json
 
 
-def template(branch, system, toplevelOnly=False, distro=None):
+def template(branch, system, toplevelOnly=False, distro=None, nixpkgs_branch=None):
+    inputs = {
+        "nix-ros-overlay": {
+            "type": "git",
+            "value": f"https://github.com/wentasah/nix-ros-overlay {branch}",
+            "emailresponsible": False
+        },
+        "system": {
+            "type": "string",
+            "value": system,
+            "emailresponsible": False
+        },
+        "distro": {
+            "type": "string",
+            "value": distro,
+            "emailresponsible": False
+        },
+    }
+    if nixpkgs_branch is not None:
+        inputs.update({
+            "nixpkgs": {
+                "type": "git",
+                "value": f"https://github.com/NixOS/nixpkgs {nixpkgs_branch}",
+                "emailresponsible": False
+            },
+        })
     return {
         "enabled": 1,
         "hidden": False,
@@ -16,31 +41,23 @@ def template(branch, system, toplevelOnly=False, distro=None):
         "enable_dynamic_run_command": False,
         "emailoverride": "",
         "keepnr": 20,
-        "inputs": {
-            "nix-ros-overlay": {
-                "type": "git",
-                "value": f"https://github.com/wentasah/nix-ros-overlay {branch}",
-                "emailresponsible": False
-            },
-            "system": {
-                "type": "string",
-                "value": system,
-                "emailresponsible": False
-            },
-            "distro": {
-                "type": "string",
-                "value": distro,
-                "emailresponsible": False
-            },
-        }
+        "inputs": inputs
     }
 
 
 jobsets = {}
 
-for branch in ['master', 'develop']:
+for job_type in ['master', 'develop', 'unstable']:
     for system in ['x86_64-linux', 'aarch64-linux']:
         for distro in ['.top', '.examples', 'noetic', 'humble', 'iron', 'jazzy', 'rolling']:
-            jobsets[f"{branch}-{distro.strip('.')}-{system.split('-')[0]}"] = template(branch, system, False, distro)
+            if job_type == 'unstable':
+                branch = "develop"
+                nixpkgs_branch = "nixos-unstable"
+            else:
+                branch = job_type
+                nixpkgs_branch = None
+            jobsets[f"{job_type}-{distro.strip('.')}-{system.split('-')[0]}"] = (
+                template(branch, system, False, distro, nixpkgs_branch)
+            )
 
 print(json.dumps(jobsets, indent=2))
